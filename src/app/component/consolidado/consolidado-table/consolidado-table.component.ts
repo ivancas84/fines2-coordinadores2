6b2f7b4d33fc7d20b188a5a12591779f1d6eb67b
unknown <ivancas84@gmail.com>
2020-02-05 07:14:13 -0300

    incorporar consultas a cursos

--- src/app/component/consolidado/consolidado-table/consolidado-table.component.ts
+++ src/app/component/consolidado/consolidado-table/consolidado-table.component.ts
@@ -2,11 +2,11 @@ import { Component, ɵclearResolutionOfComponentResourcesQueue, OnInit } from '@
 import { TableComponent } from '@component/table/table.component';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { isEmptyObject } from '@function/is-empty-object.function';
-import { forkJoin, Observable } from 'rxjs';
+import { forkJoin, Observable, Subject, BehaviorSubject } from 'rxjs';
 import { Display } from '@class/display';
 import { arrayGroupValue } from '@function/array-group-value';
 import { arrayColumn } from '@function/array-column';
-import { map } from 'rxjs/operators';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-consolidado-table',
@@ -15,33 +15,33 @@ import { map } from 'rxjs/operators';
 export class ConsolidadoTableComponent extends TableComponent implements OnInit {
 
   readonly entityName = 'comision';
-  curso_$: Observable<any>;
+  curso_$ = {};
 
   constructor(protected dd: DataDefinitionService) {
     super();
   }
 
   ngOnInit(): void {
-    this.data$.pipe(map(
+    this.data$.subscribe(
       data => {
         var ids = arrayColumn(data,"id");
-        console.log(ids);
+        ids.forEach(element => {
+          this.curso_$[element] = new BehaviorSubject(null);
+        });
+
         var display = new Display();
         display.condition.push("comision","=",ids);
-        //this.curso_$ = 
-
-        //console.log(data);
-
-        //console.log("teset");
-        // this.comisiones = arrayColumn(data,"comision");    
-        // this.cursos = arrayGroupValue(data,"comision");    
-        //return data;
+        display.size = 0;
+        this.dd.all("curso", display).subscribe(
+          curso_ => {
+            curso_.forEach(element => {
+              var v = element["comision"];
+              this.curso_$[v].next(element);
+            });
+          }
+        )
       }
-    ));
-    
-
-    //
-    //throw new Error("Method not implemented.");
+    );
   }
 
 
