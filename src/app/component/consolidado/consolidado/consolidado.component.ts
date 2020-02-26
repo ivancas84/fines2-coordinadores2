4797fae72ebfc7611c01b037cf31fd7ab82e7116
ivan <ivancas84@gmail.com>
2020-02-26 15:15:37 -0300

    implementar grid system en el consolidado

--- src/app/component/consolidado/consolidado/consolidado.component.ts
+++ src/app/component/consolidado/consolidado/consolidado.component.ts
@@ -32,7 +32,7 @@ export class ConsolidadoComponent extends ShowComponent {
     }
 
     if(!this.display.params.hasOwnProperty("fecha_anio")) this.display.params["fecha_anio"] = "2020";
-    if(!this.display.params.hasOwnProperty("fecha_anio")) this.display.params["fecha_anio"] = "2020";
+    //if(!this.display.params.hasOwnProperty("fecha_anio")) this.display.params["fecha_anio"] = "2020";
     this.condition$.next(this.display.condition);
     this.params$.next(this.display.params);
   }
